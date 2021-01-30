dc01c941d5167fb9898c581ccaf7b1ccbbc2acdb
LoredCast <manuel@Manuel-Laptop.localdomain>
2021-01-30 11:29:48 +0100

    initial commit

---
+++ electron/electron.ts
@@ -0,0 +1,43 @@
+import path from 'path'
+import isDev from'electron-is-dev'
+import { app, BrowserWindow } from 'electron'
+
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow;
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
+
+    private static onClose() {
+        // Dereference the window object. 
+        //Main.mainWindow = null;
+        console.log("closed")
+    }
+
+    private static onReady() {
+        Main.mainWindow = new Main.BrowserWindow({ width: 800, height: 600 });
+        Main.mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+        Main.mainWindow.on('closed', Main.onClose);
+        console.log("hi")
+    }
+
+    static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        // we pass the Electron.App object and the  
+        // Electron.BrowserWindow into this function 
+        // so this class has no dependencies. This 
+        // makes the code easier to write tests for 
+        Main.BrowserWindow = browserWindow;
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('ready', Main.onReady);
+        console.log("Changed Again")
+    }
+}
+
+
+Main.main(app, BrowserWindow)
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import { useRef, useState, useEffect } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface ExtendedAudioElement extends HTMLAudioElement {
+	setSinkId: (sinkId: string) => Promise<void>;
+}
+
+const App : React.FunctionComponent = () => {
+    const audioRef = useRef<ExtendedAudioElement>(null)
+   
+    const [outputs, setOutputs] = useState<MediaDeviceInfo[]>()
+    const [selectedOutput, setSelectedOutput] = useState<string>('default')
+
+
+    const setupAudio = () => {
+        navigator.getUserMedia({video:false, audio:true}, (stream) => {
+            let obj = audioRef.current!
+            obj.srcObject = stream
+
+        }, (e) => {
+            console.log(e)
+        })
+    }
+
+    const handleOutputChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedOutput(event.currentTarget.value)
+        let obj = audioRef.current!
+        obj.setSinkId(event.currentTarget.value)
+    }
+
+    useEffect(() => {
+        navigator.mediaDevices.enumerateDevices()
+            .then( devices => {
+                devices = devices.filter((output) => output.kind === "audiooutput")
+                setOutputs(devices)
+            })
+    }, []) 
+
+    return(
+        <div>
+            <h1>Webcam</h1>
+            <select onChange={ handleOutputChange }>
+            {outputs && outputs.map((output, index) => 
+                <option key={index} value={ output.deviceId }>{ output.label }</option>  
+            )}
+            </select>
+            <p>Selected Device: {selectedOutput} </p>
+            <audio ref={audioRef} controls>
+            <source src="/home/manuel/Desktop/untitled.mp3" type="audio/mpeg"/>
+            </audio>
+            </div>
+    )
 }
 
 export default App;
+
